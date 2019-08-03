db7634619adac26345dc59d841a6f70f0dca5d23
Jakob Kaivo <jkk@ung.org>
2019-08-03 08:35:54 -0400

    update license; clean up code

--- logger.c
+++ logger.c
@@ -1,51 +1,62 @@
 /*
  * UNG's Not GNU
- * 
- * Copyright (c) 2011, Jakob Kaivo <jakob@kaivo.net>
- * 
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
+ * Copyright (c) 2011-2019, Jakob Kaivo <jkk@ung.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
+#define _XOPEN_SOURCE 700
+#include <limits.h>
+#include <locale.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
-#include <limits.h>
+#include <unistd.h>
 
-// GNU libc unsets ARG_MAX
 #ifndef ARG_MAX
 #define ARG_MAX _POSIX_ARG_MAX
 #endif
 
-const char *logger_desc = "log messages";
-const char *logger_inv = "logger string...";
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char msg[ARG_MAX];
-	int i = 1;
+	setlocale(LC_ALL, "");
 
-	if (argc < 2)
+	while (getopt(argc, argv, "") != -1) {
 		return 1;
-
-	memset(msg, 0, ARG_MAX);
-
-	while (i < argc) {
-		strcat(msg, argv[i]);
-		if (i < argc)
-			strcat(msg, " ");
-		i++;
 	}
+
+	if (optind >= argc) {
+		return 1;
+	}
+
+	char msg[ARG_MAX];
+	char *p = msg;
+
+	while (optind < argc) {
+		p = stpcpy(p, argv[optind]);
+		p = stpcpy(p, " ");
+		optind++;
+	}
+
+	/* remove final space */
+	*(--p) = '\0';
 
 	syslog(LOG_USER, "%s", msg);
 
